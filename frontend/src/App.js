ce07476204d321260b3b7ec9fe95a05899df85dd
introdesign <introdesign102@gmail.com>
2026-02-18 09:18:14 +0800

    commited change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,4 @@
-import "./App.css";
-import { useState } from "react";
-import AdminConsole from "./components/AdminConsole";
-import LoginForm from "./components/LoginForm";
+
 import MeowSearch from "./components/MeowSearch";
 
 function App() {
